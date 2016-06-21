abc38d2e2069fd3cdf3f4fd385ef24bf5d89f584
falsandtru <falsandtru@users.noreply.github.com>
2016-06-21 22:54:15 +0900

    test(power-assert): update babel to 6

--- karma-espower-preprocessor/karma.conf.js
+++ karma-espower-preprocessor/karma.conf.js
@@ -7,13 +7,10 @@ module.exports = function (config) {
         frameworks: ['mocha', 'expect'],
         files: [
             { pattern: 'node_modules/requirejs/require.js', included: false },
-            { pattern: 'test/fixture/amd.html', watched: true, served: true, included: false },
             { pattern: 'build/power-assert.js', watched: true, served: true, included: true },
             { pattern: 'test/tobe_instrumented/assertion.js', watched: true, served: true, included: true },
             { pattern: 'test/tobe_instrumented/assertion.es6.js', watched: true, served: true, included: true },
-            { pattern: 'test/tobe_instrumented/customization.js', watched: true, served: true, included: true },
-            { pattern: 'test/not_tobe_instrumented/not_instrumented.js', watched: true, served: true, included: true },
-            { pattern: 'test/tobe_instrumented/modules.js', watched: true, served: true, included: true }
+            { pattern: 'test/tobe_instrumented/customization.js', watched: true, served: true, included: true }
         ],
         preprocessors: {
             'test/tobe_*/*.js': ['espower']
